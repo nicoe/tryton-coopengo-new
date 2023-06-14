b075f3a0c8eaa86038ef4c9b6b949a815aa1b5b7
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:06:14 +0200

    [sao][Guardrail] secure group.remove() in _set_value() of O2M [CUSTOM]
    Guard against undefined group in O2M _set_value() to prevent crash when
    group.remove() is called on a record that has already been removed. Adds
    null check before removal and simplifies the cleanup logic in common.js.
    
    Files: sao/src/common.js
    Extraction ref: SA5-C
    Source commits: 76da832 (secure group.remove() in _set_value() of O2M)

--- sao/src/common.js
+++ sao/src/common.js
@@ -2192,12 +2192,9 @@
                 },
                 'float': function() {
                     var factor = Number(field.factor || 1);
-                    try {
-                        var result = atof(value);
-                    } catch (e) {
-                        return null;
-                    }
-                    if (isNaN(result)) {
+                    var result = Number(value);
+                    if (isNaN(result) || value === '' ||
+                        value === null || value === undefined) {
                         return null;
                     } else {
                         return result / factor;
@@ -2387,7 +2384,7 @@
                     return Sao.common.timedelta.format(value, converter);
                 },
                 'many2one': function() {
-                    if (value === null) {
+                    if (value === null || value === undefined) {
                         return '';
                     } else {
                         return value;
@@ -2403,7 +2400,7 @@
                 var func = converts[field.type];
                 if (func) {
                     return this.quote(func(value));
-                } else if (value === null) {
+                } else if (value === null || value === undefined) {
                     return '';
                 } else {
                     return this.quote(value, _quote_empty);
