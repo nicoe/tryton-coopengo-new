153e110a90cd7af89139f1f292506edc0d580c75
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:29 +0200

    [trytond][ANALYSE] Load static files in werkzeug [CUSTOM]
    Add static file serving support in Werkzeug dev server. Extends
    bin/trytond to mount static file directories when running in
    development mode, and adds config.py option for specifying static
    file paths. Enables serving SAO and other static assets without
    a separate web server.
    
    Files: bin/trytond, config.py
    Extraction ref: MC1 (static files in bin/trytond)

--- trytond/trytond/config.py
+++ trytond/trytond/config.py
@@ -58,7 +58,9 @@ class TrytonConfigParser(configparser.ConfigParser):
         self.add_section('web')
         self.set('web', 'listen', 'localhost:8000')
         self.set('web', 'root_path', '/')
-        self.set('web', 'root', os.path.join(os.path.expanduser('~'), 'www'))
+        self.set('web', 'root',
+            os.environ.get('TRYTOND_WEB_ROOT',
+                os.path.join(os.path.expanduser('~'), 'www')))
         self.set('web', 'num_proxies', '0')
         self.set('web', 'cache_timeout', str(60 * 60 * 12))
         self.add_section('database')
