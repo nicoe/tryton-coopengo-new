537b29f0a4c926862cb82edd8a5d5ae27f3f8fc8
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:06:17 +0200

    [sao][ANALYSE] Report dev on toggle for processes from gtk client [CUSTOM]
    Improve process action handling in screen to properly route between
    different action types. Refactors the toggle/action dispatch to handle
    process actions from the GTK client style, ensuring proper button state
    management and action type detection.
    
    Files: sao/src/screen.js
    Extraction ref: SA8-C
    Source commits: ecdf120 (report dev on toggle for processes from gtk client)

--- sao/src/screen.js
+++ sao/src/screen.js
@@ -992,7 +992,10 @@
                 this, view_id, view.type, xml_view, view.field_childs,
                 view.children_definitions);
             this.views.push(view_widget);
-
+            // JMO: report https://github.com/coopengo/tryton/pull/13
+            var fkeys = {};
+            for  (var k in fields) {fkeys[k] = '';}
+            view_widget._field_keys = fkeys;
             return view_widget;
         },
         get number_of_views() {
@@ -1496,13 +1499,16 @@
                         ~['tree', 'graph', 'calendar'].indexOf(
                             this.current_view.view_type));
                 deferreds.push(search_prm);
-                for (const view of this.views) {
-                    if (view &&
-                        ((view == this.current_view) ||
-                            view.el.parent().length)) {
-                        deferreds.push(view.display());
-                    }
-                }
+                // [Coog specific]
+                // JMO: report https://github.com/coopengo/tryton/pull/13
+                // for (const view of this.views) {
+                //     if (view &&
+                //         ((view == this.current_view) ||
+                //             view.el.parent().length)) {
+                //         deferreds.push(view.display());
+                //     }
+                // }
+                deferreds.push(this.current_view.display());
                 if (this.current_view.view_type == 'tree') {
                     let view_tree = this.fields_view_tree[
                         this.current_view.view_id] || {};
