abfd3b143edf5bbce32330c4d43c66d7a3c5a060
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:16 +0200

    [sao][ANALYSE] Propagate changed status to parent on instance button activation (#131) [CUSTOM]
    Propagate the "changed" status to parent records when an instance button
    is activated on a sub-record. This ensures the parent form correctly
    detects unsaved changes after button actions modify child records.
    
    Files: sao/src/model.js
    Extraction ref: SA5-C
    Source commits: 8c01644 (propagate changed status to parent on instance button activation)
    
    Ref: #131

--- sao/src/model.js
+++ sao/src/model.js
@@ -1541,6 +1541,15 @@
                 }
             }
             this.destroyed = true;
+        },
+        _set_modified: function() {
+            var parent = this.group.parent;
+            if (parent) {
+                parent._changed[this.group.child_name] = true;
+                parent.model.fields[this.group.child_name].changed(parent);
+                parent.validate(null, true, false, true);
+                parent._set_modified();
+            }
         }
     });
 
