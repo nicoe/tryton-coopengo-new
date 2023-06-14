ce885fee8d66b1ebdff41683d0755780c0001c85
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:03:49 +0200

    [trytond][Main] Specify test cache name [CUSTOM]
    Specify explicit cache names in test infrastructure to avoid conflicts
    between test runs. Modifies test_tryton.py and tools.py to use
    deterministic cache identifiers.
    
    Files: tests/test_tryton.py, tests/tools.py
    Extraction ref: C51 (partial)
    Original PR: #81

--- trytond/trytond/tests/test_tryton.py
+++ trytond/trytond/tests/test_tryton.py
@@ -83,13 +83,13 @@ DB_CACHE_JOBS = os.environ.get('DB_CACHE_JOBS', str(_cpu_count()))
 TEST_NETWORK = bool(int(os.getenv('TEST_NETWORK', 1)))
 
 
-def activate_module(modules, lang='en'):
+def activate_module(modules, lang='en', cache_name=None):
     '''
     Activate modules for the tested database
     '''
     if isinstance(modules, str):
         modules = [modules]
-    name = '-'.join(modules)
+    name = cache_name or '-'.join(modules)
     if lang != 'en':
         name += '--' + lang
     if not db_exist(DB_NAME) and restore_db_cache(name):
@@ -372,6 +372,7 @@ class _DBTestCase(TestCase):
     module = None
     extras = None
     language = 'en'
+    cache_name = None
 
     @classmethod
     def setUpClass(cls):
@@ -380,7 +381,7 @@ class _DBTestCase(TestCase):
         modules = [cls.module]
         if cls.extras:
             modules.extend(cls.extras)
-        activate_module(modules, lang=cls.language)
+        activate_module(modules, lang=cls.language, cache_name=cls.cache_name)
 
     @classmethod
     def tearDownClass(cls):
