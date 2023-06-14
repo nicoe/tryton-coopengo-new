5ca02bcbc6f2b336ed31c1a7f35894574fd38a2e
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:46 +0200

    [trytond][Main] Add basic opentelemetry tooling for tracing [CUSTOM] (#236)
    Add OpenTelemetry integration for distributed tracing. New
    opentelemetry.py module (100 lines) with span creation and context
    propagation. Hooks into jsonrpc.py for RPC span tracking and wsgi.py
    for request-level tracing. PostgreSQL backend gets query-level spans.
    
    Files: opentelemetry.py, protocols/jsonrpc.py, wsgi.py, backend/postgresql/database.py
    Extraction ref: C7
    Original PR: #236

--- trytond/trytond/protocols/jsonrpc.py
+++ trytond/trytond/protocols/jsonrpc.py
@@ -15,6 +15,7 @@ from trytond.exceptions import (
     RateLimitException, TrytonException, UserWarning)
 from trytond.protocols.wrappers import GzipStream, Request, Response
 from trytond.tools import cached_property
+from trytond.opentelemetry import OPENTELEMETRY_ENABLED
 
 
 class JSONDecoder(object):
@@ -123,6 +124,20 @@ JSONEncoder.register(Decimal,
 class JSONRequest(Request):
     parsed_content_type = 'json'
 
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        if OPENTELEMETRY_ENABLED:
+            from opentelemetry import trace
+            current_span = trace.get_current_span()
+            current_span.set_attribute("rpc.system", "jsonrpc")
+            if self.rpc_method and '.' in self.rpc_method:
+                rpc_model, rpc_method = self.rpc_method.rsplit('.', maxsplit=1)
+            else:
+                rpc_model, rpc_method = '', str(self.rpc_method)
+            current_span.set_attribute("rpc.service", rpc_model)
+            current_span.set_attribute("rpc.method", rpc_method)
+
     @cached_property
     def parsed_data(self):
         if self.parsed_content_type in self.environ.get('CONTENT_TYPE', ''):
@@ -184,6 +199,13 @@ class JSONProtocol:
         if (isinstance(request, JSONRequest)
                 and set(parsed_data.keys()) == {'id', 'method', 'params'}):
             response = {'id': parsed_data.get('id', 0)}
+
+            if OPENTELEMETRY_ENABLED:
+                from opentelemetry import trace
+                current_span = trace.get_current_span()
+                current_span.set_attribute(
+                    "rpc.jsonrpc.request_id", response['id'])
+
             if isinstance(data, TrytonException):
                 response['error'] = data.args
             elif isinstance(data, Exception):
