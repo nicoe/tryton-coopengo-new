6621b1d7c6aa860a192fbf72d44d042be7166654
Jean Cavallo <jean.cavallo@hotmail.fr>
2024-10-23 17:01:15 +0200

    [sao][UI] Allow to vertically resize lists [CUSTOM] (#322)
    Allow vertical resizing of O2M and M2M list widgets in form views by
    adding CSS resize handles when the list does not have a fixed height.
    Gives users control over the visible area of embedded lists.
    
    Files: sao/src/view/form.js
    Extraction ref: SA7-C (vertical resize), SA5-C (O2M/M2M form handling)
    Source commits: 1511c0f (allow to vertically resize lists)
    
    Ref: #322

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -4641,6 +4641,10 @@ function eval_pyson(value){
                         .find('.treeview,.list-form').first()
                         .css('min-height', this.attributes.height + 'px')
                         .css('max-height', this.attributes.height + 'px');
+                } else {
+                    this.screen.current_view.el
+                        .find('.treeview,.list-form').first()
+                        .css('resize', 'vertical')
                 }
                 return this.screen.display();
             }.bind(this);
@@ -5229,6 +5233,10 @@ function eval_pyson(value){
                         .find('.treeview,.list-form').first()
                         .css('min-height', this.attributes.height + 'px')
                         .css('max-height', this.attributes.height + 'px');
+                } else {
+                    this.screen.current_view.el
+                        .find('.treeview,.list-form').first()
+                        .css('resize', 'vertical')
                 }
                 return this.screen.display();
             }.bind(this);
