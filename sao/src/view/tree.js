3170338ad194d5cce8ee9674b50640d7d98542d4
Nicolas Évrard <nicoe@users.noreply.github.com>
2025-01-23 17:58:35 +0100

    [sao][TO DROP] Prevent forced reflow in tree when setting column value (#384)
    Prevent forced browser reflow in tree views when setting column values.
    jQuery's .text() calls .empty() unconditionally, triggering a DOM
    mutation even when the text is unchanged. Replace with a guard that
    checks textContent before updating, significantly reducing layout
    thrashing on large lists.
    
    Files: sao/src/view/tree.js
    Extraction ref: SA7-P
    Source commits: 98807fd
    
    Ref: #384
    
    TO DROP: Probably useless because even if the reflow does indeed happen
    it's not a synchronous one (cfr https://stackoverflow.com/questions/66576476/why-is-setting-textcontent-triggering-reflow)

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2590,7 +2590,9 @@
                     if (!value) {
                         value = field.get_client(record) || '';
                     }
-                    cell.text(value);
+                    if (cell.text() != value) {
+                        cell.text(value);
+                    }
                 }
             };
             let prm = jQuery.when();
@@ -2685,7 +2687,9 @@
         },
         update_text: function(cell, record) {
             var text = this.get_textual_value(record);
-            cell.text(text).attr('title', text);
+            if (text != cell.text()) {
+                cell.text(text).attr('title', text);
+            }
         },
         render: function(record, cell) {
             if (!cell) {
@@ -2883,7 +2887,9 @@
             if (!this.tree.editable &&
                     (this.field.name + ':string' in record._values)) {
                 var text_value = this.get_textual_value(record);
-                cell.text(text_value).attr('title', text_value);
+                if (cell.text() != text_value) {
+                    cell.text(text_value).attr('title', text_value);
+                }
             } else {
                 this.update_selection(record, () => {
                     var value = this.field.get(record);
@@ -2904,7 +2910,9 @@
                         prm = jQuery.when(text);
                     }
                     prm.done(text_value => {
-                        cell.text(text_value).attr('title', text_value);
+                        if (cell.text() != text_value) {
+                            cell.text(text_value).attr('title', text_value);
+                        }
                     });
                 });
             }
@@ -2946,11 +2954,15 @@
             if (!this.tree.editable &&
                     (this.field_name + ':string' in record._values)) {
                 var text_value = this.get_textual_value(record);
-                cell.text(text_value).attr('title', text_value);
+                if (cell.text() != text_value) {
+                    cell.text(text_value).attr('title', text_value);
+                }
             } else {
                 this.update_selection(record, () => {
                     var text_value = this.get_textual_value(record);
-                    cell.text(text_value).attr('title', text_value);
+                    if (cell.text() != text_value) {
+                        cell.text(text_value).attr('title', text_value);
+                    }
                 });
             }
         },
@@ -3008,7 +3020,9 @@
             cell.unbind('click');
             this.update_selection(record, () => {
                 var text = this.get_textual_value(record);
-                cell.text(text).attr('title', text);
+                if (cell.text() != text) {
+                    cell.text(text).attr('title', text);
+                }
                 cell.click(event => {
                     event.stopPropagation();
                     var value = this.field.get(record);
@@ -3091,7 +3105,10 @@
         },
         update_text: function(cell, record) {
             var text = this.get_textual_value(record);
-            cell.children('span').text(text).attr('title', text);
+            let span = cell.children('span')
+            if (span.text() != text) {
+                span.text(text).attr('title', text);
+            }
             var button = cell.children('button');
             if (!button.length) {
                 button = jQuery('<button/>', {
@@ -3227,8 +3244,10 @@
             var value = this.field.get(record) || 0;
             var progressbar = cell.find('.progress-bar');
             progressbar.attr('aria-valuenow', value * 100);
-            progressbar.css('width', value * 100 + '%');
-            progressbar.text(text).attr('title', text);
+            if (progressbar.text() != text) {
+                progressbar.css('width', value * 100 + '%');
+                progressbar.text(text).attr('title', text);
+            }
         }
     });
 
