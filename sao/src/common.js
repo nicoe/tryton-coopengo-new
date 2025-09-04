6afad001deb6062b4f8d04d588f3f2dec0e54a6b
Nicolas Évrard <nicoe@users.noreply.github.com>
2025-09-04 10:19:32 +0200

    [sao][TO DROP] Allow to reorder tabs (#479)
    Implement drag-to-reorder on the main tab bar using HTML5 drag events.
    Tabs can be repositioned by dragging; drop logic updates the DOM order.
    Adds a grabbing cursor style for visual feedback.
    
    Fix CCOOG-1246
    
    Files: common.js, sao.less, tab.js
    Extraction ref: SA9-P
    Source commits: 0048c6b, 973f424
    
    Ref: #479

--- sao/src/common.js
+++ sao/src/common.js
@@ -4537,4 +4537,7 @@
         }
     };
 
+    Sao.common.transparent_png = document.createElement('img');
+    Sao.common.transparent_png.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
 }());
