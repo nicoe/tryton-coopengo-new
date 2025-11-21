2f9ed66c450c6fba5f0f94ee249eabcfc2866144
Nicolas Évrard <nicoe@users.noreply.github.com>
2025-11-21 09:40:11 +0100

    [sao][TO DROP] Escape record name in completion [PREVIEW] (#533)
    https://bugs.tryton.org/14363
    
    Escape the record name (content.name) in the completion dropdown to
    prevent HTML entities in record names from being interpreted as markup.
    
    Files: common.js
    Extraction ref: SA4-P
    Source commits: a8fecba
    
    Ref: #533

--- sao/src/common.js
+++ sao/src/common.js
@@ -3935,7 +3935,7 @@
         },
         _format: function(content) {
             if (this.format) {
-                content = this.format(content);
+                return this.format(content);
             }
             if (content instanceof jQuery) {
                 return content;
@@ -4004,7 +4004,7 @@
     Sao.common.get_completion = function(el, source,
             match_selected, action_activated, search=true, create=true) {
         var format = function(content) {
-            return content.name;
+            return jQuery('<span/>').text(content.name);
         };
         var completion = new Sao.common.InputCompletion(
                 el, source, match_selected, format);
