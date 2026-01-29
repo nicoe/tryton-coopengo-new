404fe083a5e7923d4db12dd604800a2e6a218fd4
Nicolas Évrard <nicoe@openhex.org>
2026-01-29 16:58:47 +0100

    [sao][Main] Add a custom billboard widget [CUSTOM]
    Add a custom billboard.js chart widget in coog.js. Provides a Coog-specific
    wrapper around the billboard charting library with custom configuration
    defaults and rendering logic for dashboard-style charts.
    
    Files: view/coog.js (new file, 59 lines)
    Extraction ref: SA10-C (custom chart widget)

--- sao/src/view/coog.js
+++ sao/src/view/coog.js
@@ -38,8 +38,67 @@
         },
     });
 
+    Sao.View.Form.Billboard = Sao.class_(Sao.View.Form.Widget, {
+        class_: 'form-billboard',
+        init: function(view, attributes) {
+            Sao.View.Form.Billboard._super.init.call(this, view, attributes);
+            this.el = jQuery('<div/>', {
+                'class': this.class_,
+            });
+            this.el.uniqueId();
+            this._x_sao = {};
+        },
+        display: function() {
+            let prm = Sao.View.Form.Billboard._super.display.call(this);
+            if (!this.field || !this.record) {
+                return prm;
+            }
+
+            let value = this.field.get_client(this.record);
+            if (!value.data) {
+                return prm;
+            }
+            this._x_sao = structuredClone(value['x-sao']);
+            if (['pie', 'donut'].includes(value.data.type)) {
+                this._remap_ids();
+            }
+            Object.assign(value, {
+                bindto: `#${this.el.attr('id')}`,
+            });
+            if (value.data && Object.keys(value.data).length != 0) {
+                value.data.onclick = this.action.bind(this);
+            }
+            let bb_node = document.getElementById(this.el.attr('id'));
+            if (bb_node && Object.keys(value).length != 0) {
+                bb.generate(value);
+            }
+            return prm;
+        },
+        action: function(data, element) {
+            if (this._x_sao && this._x_sao.action) {
+                let ids = this._x_sao.ids[data.id][data.index];
+                let ctx = Object.assign({}, this.view.screen.local_context, this._x_sao.context || {});
+                ctx.data_id = data.id;
+                delete ctx.active_ids;
+                delete ctx.active_id;
+                Sao.Action.execute(this._x_sao.action, {
+                    id: ((ids && ids.length > 0) ? ids[0] : null),
+                    ids: ids,
+                }, ctx, false);
+            }
+        },
+        _remap_ids: function() {
+            for (let [idx, value] of Object.values(this._x_sao.ids).entries()) {
+                for (let i = 0; i < idx; i++) {
+                    value.splice(0, 0, undefined);
+                }
+            }
+        },
+    });
+
     Object.assign(Sao.View.FormXMLViewParser.WIDGETS, {
         'json': Sao.View.Form.JSON,
+        'billboard': Sao.View.Form.Billboard,
     });
 
 }());
