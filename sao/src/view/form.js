5f029170209ec7146f3b0255611aa2c8fc528a1d
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-23 20:53:27 +0100

    [sao][Main] Multi-Mixed view [CUSTOM]
    Implement multi_mixed_view support for O2M/M2M widgets. When a field has
    multiple view definitions with children_definitions, the widget creates
    synchronized sub-views that display different record types from the same
    group. Includes view.js parse changes for children_definitions, tree.js
    n_children/is_leaf handling, and form.js O2M display logic for mixed views.
    
    Fix #11637
    
    Files: screen.js, view.js, view/form.js, view/tree.js
    Extraction ref: SA5-C / SA-group_sync-C (multi_mixed_view core)
    Source commits: 59172a9 (SA5 split), 76da832, 41df5e2, 785643c

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -304,6 +304,7 @@ function eval_pyson(value){
             });
             this.notebooks = [];
             this.expandables = [];
+            this.widget_groups = {};
             this.containers = [];
             this.widget_id = 0;
             Sao.View.Form._super.init.call(this, view_id, screen, xml);
@@ -3486,6 +3487,25 @@ function eval_pyson(value){
             this.screen.windows.push(this);
             this.prm = this.screen.switch_view().done(() => {
                 this.content.append(this.screen.screen_container.el);
+                // [Coog specific]
+                // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+                if (attributes.group) {
+                    this.screen._multiview_form = view;
+                    this.screen._multiview_group = attributes.group;
+                    if (!Object.hasOwn(view.widget_groups, attributes.group)) {
+                        view.widget_groups[attributes.group] = [];
+                    }
+                    var wgroup = view.widget_groups[attributes.group];
+                    if (this.screen.current_view.view_type == 'tree') {
+                        if ((wgroup.length > 0) &&
+                            (wgroup[0].screen.current_view.view_type == 'tree')) {
+                            throw new Error("Wrong definition");
+                        }
+                        wgroup.unshift(this);
+                    } else {
+                        wgroup.push(this);
+                    }
+                }
             });
 
             if (attributes.add_remove) {
@@ -3497,6 +3517,113 @@ function eval_pyson(value){
         },
         get _styled_el() {
             return null;
+        },
+        // [Coog specific]
+        // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+        group_sync: function(screen, current_record){
+            if (this.attributes.mode == 'form')
+                return;
+            if (!this.view || !this.view.widgets)
+                return;
+
+            function is_compatible(screen, record){
+                if (!screen.current_view)
+                    return false;
+
+                return (!(screen.current_view.view_type == 'form' &&
+                    record &&
+                    screen.model_name != record.model.name));
+            }
+
+            var key;
+            var record;
+            var widget;
+            var widgets = this.view.widgets[this.field_name];
+            var to_sync = [];
+
+            // !!!> get a list of widgets affected by the new record
+            for (var j = 0; j < widgets.length; j++){
+                widget = widgets[j];
+                if (!widget.hasOwnProperty('attributes')){
+                    return;
+                }
+
+                if (widget == this ||
+                    widget.attributes.group != this.attributes.group ||
+                    !widget.hasOwnProperty('screen')){
+                    continue;
+                }
+
+                if (widget.screen.current_record == current_record){
+                    continue;
+                }
+
+                record = current_record;
+                if (!is_compatible(widget.screen, record))
+                    record = null;
+                if (!widget.validate())
+                    return;
+
+                to_sync.push({'widget': widget, 'record': record});
+            }
+            widget = null;
+            var to_display = null;
+            var to_display_prm = jQuery.when();
+            var record_load_promises, display_prm;
+
+            function display_form(widget, record) {
+                return function () {
+                    widget.display(widget.record, widget.field);
+                };
+            }
+
+            // !!!> add fields; change widget's record; display widgets
+            for (var i = 0; i < to_sync.length; i++){
+                widget = to_sync[i].widget;
+                record = to_sync[i].record;
+                record_load_promises = [];
+
+                if (!widget.screen.current_view)
+                    continue;
+
+                // !!!> add widget's fields to the record
+                if (widget.screen.current_view.view_type == 'form' &&
+                    record &&
+                    widget.screen.group.model.name == record.group.model.name){
+                    var fields = widget.screen.group.model.fields;
+                    // !!!> format fields for method "add_fields"
+                    var ret = [];
+                    for(var name in fields){
+                        ret[name] = fields[name].description;
+                    }
+                    // !!!> initiate and add new fields
+                    record.group.model.add_fields(ret);
+
+                    for (var field_name in fields) {
+                        if (!fields.hasOwnProperty(field_name)) {
+                            continue;
+                        }
+                        record_load_promises.push(record.load(field_name));
+                    }
+                }
+
+                widget.screen.current_record = record;
+                display_prm = jQuery.when.apply(jQuery, record_load_promises);
+                display_prm.done(display_form(widget, record).bind(this));
+                if (record){
+                    to_display = widget;
+                    to_display_prm = display_prm;
+                }
+            }
+            if (to_display) {
+                to_display_prm.done(function() {
+                    for (var j in to_display.view.containers) {
+                        var container = widget.view.containers[j];
+                        container.resize();
+                    }
+                    to_display.display(to_display.record, to_display.field);
+                });
+            }
         },
         get_access: function(type) {
             var model = this.attributes.relation;
@@ -3531,6 +3658,110 @@ function eval_pyson(value){
         },
         get modified() {
             return this.screen.current_view.modified;
+        },
+        group_sync: function(screen, current_record){
+            if (this.attributes.mode == 'form')
+                return;
+            if (!this.view || !this.view.widgets)
+                return;
+
+            function is_compatible(screen, record){
+                if (screen.current_view === undefined)
+                    return false;
+
+                return (!(screen.current_view.view_type == 'form' &&
+                    record !== undefined &&
+                    screen.model_name != record.model.name));
+            }
+
+            var key;
+            var record;
+            var widget;
+            var widgets = this.view.widgets[this.field_name];
+            var to_sync = [];
+
+            // !!!> get a list of widgets affected by the new record
+            for (var j = 0; j < widgets.length; j++){
+                widget = widgets[j];
+                if (!widget.hasOwnProperty('attributes')){
+                    return;
+                }
+
+                if (widget == this ||
+                    widget.attributes.group != this.attributes.group ||
+                    !widget.hasOwnProperty('screen')){
+                    continue;
+                }
+
+                if (widget.screen.current_record == current_record){
+                    continue;
+                }
+
+                record = current_record;
+                if (!is_compatible(widget.screen, record))
+                    record = null;
+                if (!widget.validate())
+                    return;
+
+                to_sync.push({'widget': widget, 'record': record});
+            }
+            widget = null;
+            var to_display = null;
+            var record_load_promises, display_prm;
+
+            function display_form(widget, record) {
+                return function () {
+                    widget.screen.current_record = record;
+                    widget.display(widget.record(), widget.field());
+                };
+            }
+
+            // !!!> add fields; change widget's record; display widgets
+            for (var i = 0; i < to_sync.length; i++){
+                widget = to_sync[i].widget;
+                record = to_sync[i].record;
+                record_load_promises = [];
+
+                if (widget.screen.current_view === undefined)
+                    continue;
+
+                // !!!> add widget's fields to the record
+                if (widget.screen.current_view.view_type == 'form' &&
+                    record !== undefined && record !== null &&
+                    widget.screen.group.model.name == record.group.model.name){
+                    var fields = widget.screen.group.model.fields;
+                    // !!!> format fields for method "add_fields"
+                    var ret = [];
+                    for(var name in fields){
+                        ret[name] = fields[name].description;
+                    }
+                    // !!!> initiate and add new fields
+                    record.group.model.add_fields(ret);
+
+                    for (var field_name in fields) {
+                        if (!fields.hasOwnProperty(field_name)) {
+                            continue;
+                        }
+                        record_load_promises.push(record.load(field_name));
+                    }
+                }
+
+                display_prm = jQuery.when.apply(jQuery, record_load_promises);
+                display_prm.then(display_form(widget, record).bind(this));
+                if (record){
+                    to_display = widget;
+                }
+            }
+            // !!!> resize forms to fix display width
+            if (widget){
+                for (j in widget.view.containers) {
+                    var container = widget.view.containers[j];
+                    container.resize();
+                }
+            }
+            if (to_display) {
+                to_display.display(to_display.record(), to_display.field());
+            }
         },
         set_readonly: function(readonly) {
             Sao.View.Form.One2Many._super.set_readonly.call(this, readonly);
@@ -3657,7 +3888,15 @@ function eval_pyson(value){
                 }
 
                 var new_group = record.field_get_client(this.field_name);
-                if (new_group != this.screen.group) {
+
+                // [Coog specific]
+                // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+                if (this.attributes.group && this.attributes.mode == 'form') {
+                    if (!this.screen.current_record) {
+                        this.set_invisible(this.visible);
+                    }
+                }
+                if (new_group && new_group != this.screen.group) {
                     this.screen.set_group(new_group);
                     if ((this.screen.current_view.view_type == 'form') &&
                         this.screen.group.length) {
@@ -3709,7 +3948,9 @@ function eval_pyson(value){
             if (!this.write_access || !this.read_access) {
                 return;
             }
-            this.view.set_value();
+            // [Coog specific]
+            // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+            // this.view.set_value();
             var domain = this.field.get_domain(this.record);
             var context = this.field.get_search_context(this.record);
             domain = [domain,
@@ -3980,7 +4221,9 @@ function eval_pyson(value){
         },
         validate: function() {
             var prm = jQuery.Deferred();
-            this.view.set_value();
+            // [Coog specific]
+            // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+            // this.view.set_value();
             var record = this.screen.current_record;
             if (record) {
                 var fields = this.screen.current_view.get_fields();
@@ -3998,6 +4241,15 @@ function eval_pyson(value){
         },
         set_value: function() {
             this.screen.current_view.set_value();
+            // [Coog specific]
+            // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
+            if (this.screen.current_view.view_type == 'form' &&
+                this.attributes.group &&
+                this.screen.model.name != this.record.model.name)
+                return;
+            if (this.screen.modified()) {  // TODO check if required
+                this.view.screen.record_modified(false);
+            }
         },
         _update_completion: function(text) {
             if (!this.record) {
