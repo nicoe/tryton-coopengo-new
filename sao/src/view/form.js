5a4184b4d12803dd928a44926218a43c76269ab0
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-23 20:56:03 +0100

    [sao][Main] expand_toolbar [CUSTOM]
    Add expand_toolbar attribute support to O2M/M2M widgets. When set, the
    widget toolbar (add/remove/open buttons) is rendered in the title bar
    area and can be toggled visible/hidden with a coog-hidden-toolbar class.
    
    Files: view/form.js
    Extraction ref: SA5-C (O2M/M2M expand_toolbar)
    Source commits: part of 59172a9 (SA5 split), 050e09f

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -4102,8 +4102,16 @@ function eval_pyson(value){
             ).appendTo(buttons);
             this.but_undel.click(disable_during(this.undelete.bind(this)));
 
+            var content_class = this.class_ + '-content panel-body';
+            // [Coog specific]
+            //      > attribute expand_toolbar (hide toolbar)
+            if (attributes.expand_toolbar) {
+                this.menu.hide();
+                content_class += ' coog-hidden-toolbar';
+            }
+
             this.content = jQuery('<div/>', {
-                'class': this.class_ + '-content panel-body'
+                'class': content_class
             });
             this.el.append(this.content);
 
@@ -5026,8 +5034,16 @@ function eval_pyson(value){
             ).appendTo(buttons);
             this.but_unremove.click(this.unremove.bind(this));
 
+            var content_class = this.class_ + '-content panel-body';
+            // [Coog specific]
+            //      > attribute expand_toolbar (hide toolbar)
+            if (attributes.expand_toolbar) {
+                this.menu.hide();
+                content_class += ' coog-hidden-toolbar';
+            }
+
             this.content = jQuery('<div/>', {
-                'class': this.class_ + '-content panel-body'
+                'class': content_class
             });
             this.el.append(this.content);
             var model = attributes.relation;
