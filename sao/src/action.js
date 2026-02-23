99fc470a14ac7dc4d5e5d652493381455d5a2670
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-23 20:41:55 +0100

    [sao][Main] Allow extra_context in wizards [CUSTOM]
    Pass extra_context through to wizard actions by reading it from the
    context object in action.js. Ensures that wizards opened from screens
    with custom context receive the expected additional parameters.
    
    Files: action.js
    Extraction ref: SA8-C (screen/action scope)
    Source commits: part of 59172a9 SA8 split (action.js extra_context)

--- sao/src/action.js
+++ sao/src/action.js
@@ -86,6 +86,8 @@
                     {}, context,
                     decoder.decode( action.pyson_context || '{}'));
                 ctx = jQuery.extend(ctx, params.context);
+                // [Coog Specific] handle extra_context
+                ctx = jQuery.extend(ctx, data.extra_context || {});
 
                 ctx.context = ctx;
                 decoder = new Sao.PYSON.Decoder(ctx);
@@ -120,6 +122,8 @@
                 params.action = action.wiz_name;
                 params.data = data;
                 params.context = context;
+                params.context = jQuery.extend(
+                    params.context, data.extra_context || {});
                 params.window = action.window;
                 name_prm = jQuery.when(action.name);
                 if ((action.keyword || 'form_action') === 'form_action') {
