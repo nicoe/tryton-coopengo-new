7568c5aea955762b59bcf2fc6e406e8b44304caf
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 09:44:36 +0100

    [trytond][ANALYSE] Wsgi fixes [CUSTOM]
    Add Gunicorn and uWSGI integration for production deployments. Creates
    gunicorn.py with post_fork cache listener setup and post_request RSS
    memory watchdog. Adds uWSGI postfork decorator support in application.py.
    Removes unused _purge_listeners method from cache.py.
    
    Files: trytond/application.py, trytond/cache.py, trytond/gunicorn.py (new)
    Extraction ref: C9 (Gunicorn/uWSGI support)

--- trytond/trytond/cache.py
+++ trytond/trytond/cache.py
@@ -496,30 +496,6 @@ class MemoryCache(BaseCache):
                 if cls._local.listeners.get(dbname) == current_thread:
                     del cls._local.listeners[dbname]
 
-    @classmethod
-    def _purge_listeners(cls, dbname):
-        '''
-        Purges all listeners for a given database
-
-        Should no longer be useful, but we may need it later so we will keep it
-        around a little longer
-        '''
-        pid = os.getpid()
-        thread_id = None
-        with cls._listener_lock[pid]:
-            if (pid, dbname) in cls._listener:
-                thread_id = cls._listener[pid, dbname].ident
-                del cls._listener[pid, dbname]
-
-        # JMO : doctest teardown remains stuck with code below
-        # TODO: fix this
-        if config.getboolean('env', 'testing'):
-            # We removed the thread from the list, but it can still be alive if
-            # it is busy clearing some cache
-            if thread_id is not None:
-                while {thread_id} & {x.ident for x in threading.enumerate()}:
-                    time.sleep(0.01)
-
 
 if config.get('cache', 'class'):
     Cache = resolve(config.get('cache', 'class'))
