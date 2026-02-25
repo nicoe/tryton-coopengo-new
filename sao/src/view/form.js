7e8b7f8eafc36753a5e075e864cfc3ecffd47d35
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 18:09:43 +0100

    [sao][PREVIEW] Allow to use link in ModelView (#565)
    https://bugs.tryton.org/14676
    
    Allow link navigation in ModelView (non-ModelStorage) forms by extracting
    the link-opening logic from Many2One into a shared helper in common.js.
    When a model is view-only, the link opens the record in a new tab using
    the model's default action.
    
    Files: common.js, view/form.js
    Extraction ref: SA5-P
    Source commits: 7f253b9
    
    Fix #PMETA-3418, #PCLAS-2121
    
    Ref: #565, #592

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -1086,11 +1086,7 @@ function eval_pyson(value){
             var data = {},
                 context = {},
                 pyson_ctx = {};
-            if (record) {
-                if (record.id < 0) {
-                    this.hide();
-                    return;
-                }
+            if (record && (record.id >= 0)) {
                 data = {
                     model: record.model.name,
                     id: record.id,
@@ -1103,6 +1099,9 @@ function eval_pyson(value){
                     active_ids: [record.id],
                 };
                 this._current = record.id;
+            } else if (record && (record.id < 0)) {
+                context = record.get_context();
+                this._current = null;
             } else {
                 this._current = null;
                 this.el.sao_hide();
@@ -1150,32 +1149,35 @@ function eval_pyson(value){
                 var current = this._current;
                 if (tab_domains.length) {
                     tab_domains.map(function(d, i) {
-                        var tab_domain = d[1];
-                        const prm = Sao.rpc({
-                            'method': (
-                                'model.' + action.res_model + '.search_count'),
-                            'params': [
-                                ['AND', domain, tab_domain], 0, 100, context],
-                        }, Sao.Session.current_session, true, false).then(
-                            value => {
-                                this._set_count(
-                                    value, i, current, counter,
-                                    action.name, tab_domains);
-                        });
-                        promesses.push(prm);
+                        let tab_domain = ['AND', domain, d[1]];
+                        let context_vars = Sao.common.domain_context_vars(tab_domain);
+                        if ((record && record.id >= 0) || !context_vars.has('active_id')) {
+                            const prm = Sao.rpc({
+                                'method': `model.${action.res_model}.search_count`,
+                                'params': [tab_domain, 0, 100, context],
+                            }, Sao.Session.current_session, true, false).then(
+                                value => {
+                                    this._set_count(
+                                        value, i, current, counter,
+                                        action.name, tab_domains);
+                            });
+                            promesses.push(prm);
+                        }
                     }, this);
                 } else {
-                    const prm = Sao.rpc({
-                        'method': (
-                            'model.' + action.res_model + '.search_count'),
-                        'params': [domain, 0, 100, context],
-                    }, Sao.Session.current_session, true, false
-                    ).then(value => {
-                        this._set_count(
-                            value, 0, current, counter,
-                            action.name, tab_domains);
-                    });
-                    promesses.push(prm);
+                    let context_vars = Sao.common.domain_context_vars(domain);
+                    if ((record && record.id >= 0) || !context_vars.has('active_id')) {
+                        const prm = Sao.rpc({
+                            'method': `model.${action.res_model}.search_count`,
+                            'params': [domain, 0, 100, context],
+                        }, Sao.Session.current_session, true, false
+                        ).then(value => {
+                            this._set_count(
+                                value, 0, current, counter,
+                                action.name, tab_domains);
+                        });
+                        promesses.push(prm);
+                    }
                 }
             }
             return jQuery.when.apply(jQuery, promesses);
