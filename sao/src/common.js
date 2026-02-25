48beaee5c1d4e21a615c961a50f34e17167b0262
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 15:21:40 +0100

    [sao][Main] Add a copy button on error and warning dialogs (#475)
    Add a "Copy" button to error and warning dialogs, allowing users to copy
    the full error message text to the clipboard for bug reports and support
    tickets. Includes hover styles for the copy button in sao.less.
    
    Fix PCLAS-1782
    
    Files: common.js, sao.less
    Extraction ref: SA11-C
    Source commits: 687a733
    
    Ref: #475

--- sao/src/common.js
+++ sao/src/common.js
@@ -3519,12 +3519,30 @@
         build_dialog: function(message, title, prm) {
             var dialog = Sao.common.WarningDialog._super.build_dialog.call(
                 this);
+            let copy_text = '&#128203; ' + Sao.i18n.gettext("Copy");
+            let copied_text = '&#9989; ' + Sao.i18n.gettext("Copied!");
+            let copy_handler = jQuery('<div/>', {
+                'class': 'copy-content',
+            }).html(copy_text).click(() => {
+                if (!navigator.clipboard) {
+                    console.log("The clipboard can only be accessed in a secure context");
+                    return
+                }
+                copy_handler.html(copy_text);
+                navigator.clipboard.writeText(title + '\n' + message).then(() => {
+                    copy_handler.html(copied_text);
+                });
+                window.setTimeout(() => {
+                    copy_handler.html(copy_text);
+                }, 1.5 * 1000);
+            });
             var content = jQuery('<div/>', {
                 'class': 'alert alert-warning',
                 role: 'alert'
             }).append(jQuery('<h4/>')
                 .text(title)
-                .css('white-space', 'pre-wrap'));
+                .css('white-space', 'pre-wrap'))
+            .append(copy_handler);
             if (message) {
                 content.append(jQuery('<span/>')
                     .text(message)
@@ -3791,21 +3809,38 @@
                     'word-break': 'break-all',
                 }));
             if (details) {
-                alert_.append(
-                    jQuery('<p/>').append(jQuery('<a/>', {
-                        'class': 'btn btn-default',
-                        role: 'button',
-                        'data-toggle': 'collapse',
-                        'data-target': '#error-detail',
-                        'aria-expanded': false,
-                        'aria-controls': '#error-detail',
-                    }).text(Sao.i18n.gettext("Details"))))
-                    .append(jQuery('<p/>', {
-                        'class': 'collapse',
-                        id: 'error-detail',
-                    }).append(jQuery('<pre/>', {
-                        'class': 'pre-scrollable',
-                    }).text(details)));
+                alert_.append(jQuery('<p/>').append(jQuery('<a/>', {
+                    'class': 'btn btn-default',
+                    role: 'button',
+                    'data-toggle': 'collapse',
+                    'data-target': '#error-detail',
+                    'aria-expanded': false,
+                    'aria-controls': '#error-detail',
+                }).text(Sao.i18n.gettext("Details"))));
+                alert_.append(jQuery('<p/>', {
+                    'class': 'collapse',
+                    id: 'error-detail',
+                }).append(jQuery('<pre/>', {
+                    'class': 'pre-scrollable',
+                }).text(details)));
+                let copy_text = '&#128203; ' + Sao.i18n.gettext("Copy");
+                let copied_text = '&#9989; ' + Sao.i18n.gettext("Copied!");
+                let copy_handler = jQuery('<div/>', {
+                    'class': 'copy-content',
+                }).html(copy_text).click(() => {
+                    if (!navigator.clipboard) {
+                        console.log("The clipboard can only be accessed in a secure context");
+                        return
+                    }
+                    copy_handler.html(copy_text);
+                    navigator.clipboard.writeText(title + '\n' + details).then(() => {
+                        copy_handler.html(copied_text);
+                    });
+                    window.setTimeout(() => {
+                        copy_handler.html(copy_text);
+                    }, 1.5 * 1000);
+                });
+                alert_.append(copy_handler);
             }
             jQuery('<a/>', {
                 'class': 'btn btn-link',
