a064b5efca5864bc9d1bd06d6374fddbe3018af6
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 15:53:38 +0100

    [sao][TO DROP] Skip parent_name field when validating a record (#501)
    Skip the parent_name field during record validation. When an on_change
    sets an x2M field without a screen, exclude_field is null, causing the
    client to try validating the parent field of new child records
    unnecessarily.
    
    Fix PCLAS-1875
    https://bugs.tryton.org/14283
    
    Files: model.js
    Extraction ref: SA5-P
    Source commits: 5f04c49
    
    Ref: #501

--- sao/src/model.js
+++ sao/src/model.js
@@ -1345,6 +1345,9 @@
                 if ([this.group.exclude_field, this.group.parent_name].includes(fname)) {
                     continue;
                 }
+                if (fname == this.group.parent_name) {
+                    continue;
+                }
                 if (!field.validate(this, softvalidation, pre_validate)) {
                     result = false;
                 }
