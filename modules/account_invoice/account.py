d8d9c22f3087e152c8f24d12a2da9b29e6c9a9f6
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-03-10 21:12:20 +0100

    [modules/account_invoice][TO ANALYSE] Call Invoice.process in RescheduleLines wizard [CUSTOM]
    Adds Invoice.process(invoices) call after rescheduling lines, ensuring invoice
    state is updated to reflect the new move.
    
    [Originally part of a combined commit (4edf4fc5b1) that also removed
    the InvoiceFutureWarning test. Kept together since both relate
    to invoice processing flow corrections.]

--- modules/account_invoice/account.py
+++ modules/account_invoice/account.py
@@ -391,6 +391,7 @@ class RescheduleLines(metaclass=PoolMeta):
         Invoice.write(invoices, {
                 'additional_moves': [('add', [move.id])],
                 })
+        Invoice.process(invoices)
         return move, balance_line
 
 
