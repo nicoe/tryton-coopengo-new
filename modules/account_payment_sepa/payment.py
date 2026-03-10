9f3a30a9480ab325438257b01f2d96bf6ee5f381
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-03-10 21:12:22 +0100

    [modules/account_payment_sepa][TO ANALYSE] Remove mandate identification uniqueness [CUSTOM]
    Removes the SQL unique constraint on SEPA mandate identification numbers.
    Needed for multi-portfolio setups where the same mandate may appear in
    different contexts. Includes test adaptation.
    
    Original: 717bf9fc9a

--- modules/account_payment_sepa/payment.py
+++ modules/account_payment_sepa/payment.py
@@ -663,10 +663,14 @@ class Mandate(Workflow, ModelSQL, ModelView):
                     },
                 })
         t = cls.__table__()
-        cls._sql_constraints = [
-            ('identification_unique', Unique(t, t.company, t.identification),
-                'account_payment_sepa.msg_mandate_unique_id'),
-            ]
+        # JMO/RSE 2017_04_18 Following 4929e02594d
+        # We override in coog the possibility to keep the mandate
+        # for several bank account but we suffer from the register order
+        # if we try to delete the constraint from coog module
+        # cls._sql_constraints = [
+        #     ('identification_unique', Unique(t, t.company, t.identification),
+        #         'account_payment_sepa.msg_mandate_unique_id'),
+        #     ]
         cls._sql_indexes.add(
             Index(
                 t, (t.state, Index.Equality(cardinality='low')),
