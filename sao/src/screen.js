2fde7c9f078edbfd514307c4af34422e8269e2a2
ThomasMossaz <thomas.mossaz@coopengo.com>
2026-04-29 17:20:08 +0200

    sao: [BUGFIX] Fix invalid domain error message (#609)

--- sao/src/screen.js
+++ sao/src/screen.js
@@ -2084,8 +2084,7 @@
                         fields.push(domain_parser.string(invalid));
                     } else {
                         fields.push(Sao.i18n.gettext(
-                            '"%1" is not valid according to its domain.'),
-                            string);
+                            '"%1" is not valid according to its domain.', string));
                     }
                 }
             }
